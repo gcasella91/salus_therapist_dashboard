368e5351aceac041d57c3f5e5761a757a25caf81
Raghu Betina <raghu@firstdraft.com>
2019-12-07 22:46:26 +0000

    Only patient user can modify patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,14 @@
 class PatientsController < ApplicationController
+  before_action :current_user_must_be_patient_therapist, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_patient_therapist
+    patient = Patient.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == patient.therapist
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @patients = Patient.all
 
