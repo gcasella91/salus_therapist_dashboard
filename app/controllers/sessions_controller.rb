114f7f43581a54b53c4e0cfeb3e24a260f72c1d1
Raghu Betina <raghu@firstdraft.com>
2019-12-07 22:46:26 +0000

    Only session user can modify session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
+  before_action :current_user_must_be_session_therapist, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_session_therapist
+    session = Session.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == session.therapist
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @sessions = Session.all
 
